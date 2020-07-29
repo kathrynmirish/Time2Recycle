d56b3bb6403344fc2b4ff78a142cba577b8730af
Kathryn <kathrynmirish@gmail.com>
2020-07-29 09:58:13 +0100

    adding in summary stats

--- score analysis.py
+++ score analysis.py
@@ -14,18 +14,33 @@ import scipy.stats as stats
 after = np.genfromtxt('after.csv',delimiter=',',usecols=(16))
 before = np.genfromtxt('before.csv',delimiter=',',usecols=(16))
 
-bcount=np.unique(before,return_counts=True)
-acount=np.unique(after,return_counts=True)
+#open file for summary data 
+file2=open('summaryscores.txt','a')
 
-bperc=bcount[1]*100/len(before)
-aperc=acount[1]*100/len(after)
+#storing summary data
+file2.write('summary data for after ')
+file2.write(str(stats.describe(after)))
+file2.write('             summary data for before ')
+file2.write(str(stats.describe(before)))
+file2.close()
 
+#open file to store results of t test 
 file=open('ttestresults.txt','a')
 
+#perform 2 sample t test
 file.write('t test p value is')
 file.write(str(stats.ttest_ind(before,after)[1]))
 file.close()
 
+#count data for scores
+bcount=np.unique(before,return_counts=True)
+acount=np.unique(after,return_counts=True)
+
+#converting count data to % of cohort as n1!=n2
+bperc=bcount[1]*100/len(before)
+aperc=acount[1]*100/len(after)
+
+#plotting bar chart of count data
 plt.bar(bcount[0],bperc,color='b',width=0.35,align='edge',label='before')
 plt.bar(acount[0],aperc,color='g',width=-0.35,align='edge',label='after')
 plt.xticks(np.arange(16))
@@ -34,3 +49,4 @@ plt.title('quiz scores before and after time 2 recycle')
 plt.legend()
 plt.savefig('quizscores')
 plt.show()
+
